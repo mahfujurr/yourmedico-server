99e10ee1b299a9e936e71a993cfed880e515646e
muradxkira <mahfujur797@gmail.com>
2022-11-07 23:49:42 +0600

    first commit on server

---
+++ index.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const cors = require('cors');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
+require('dotenv').config();
+
+const app = express();
+const port = process.env.PORT || 5000;
+
+// middle wares
+app.use(cors());
+app.use(express.json());
+
+app.get('/', (req, res) => {
+    res.send('YourMedico server is running')
+})
+
+app.listen(port, () => {
+    console.log(`yourMedico server running on ${port}`);
+})
